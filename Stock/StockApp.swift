9d596d914331d7c27e1ee9425e4b6c024e205a6e
NurseyitTursunkulov <nurseyit.tursunkulov@gmail.com>
2020-12-11 09:12:54 +0600

    Initial Commit

---
+++ Stock/StockApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockApp.swift
+//  Stock
+//
+//  Created by Nurseyit on 12/11/20.
+//
+
+import SwiftUI
+
+@main
+struct StockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
